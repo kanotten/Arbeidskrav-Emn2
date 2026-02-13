b767151c25b877c5850bac4add1605be2f697842
Kenneth Sheikh <kenneth.shekikh@hotmail.com>
2026-02-13 16:06:42 +0100

    fixed meny logic

--- BibliotekSystem/Program.cs
+++ BibliotekSystem/Program.cs
@@ -15,8 +15,67 @@ class Program
         bibliotek.RegistrerBruker(ansatt);
         bibliotek.RegistrerBruker(medlem);
 
-        Bok bok1 = new Bok("Haien", 1974, "Peter Benchley", 300);
+        Bok bok1 = new Bok("Haien", 1995, "Kenneth Sheikh", 300);
         bibliotek.LeggTilMedia(bok1, ansatt);
+
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\n1. Vis tilgjengelige medier");
+            Console.WriteLine("2. Lån media");
+            Console.WriteLine("3. Lever media");
+            Console.WriteLine("4. Avslutt");
+            Console.Write("Velg: ");
+
+            string valg = Console.ReadLine();
+
+            try
+            {
+                switch (valg)
+                {
+                    case "1":
+                        bibliotek.VisTilgjengeligeMedier();
+                        break;
+
+                    case "2":
+                        Console.Write("MediaID: ");
+                        string mediaID = Console.ReadLine();
+
+                        Console.Write("BrukerID: ");
+                        string brukerID = Console.ReadLine();
+
+                        bibliotek.LånMedia(mediaID, brukerID);
+                        Console.WriteLine("Lån registrert.");
+                        break;
+
+                    case "3":
+                        Console.Write("MediaID: ");
+                        string mediaID2 = Console.ReadLine();
+
+                        Console.Write("BrukerID: ");
+                        string brukerID2 = Console.ReadLine();
+
+                        bibliotek.LeverInnMedia(mediaID2, brukerID2);
+                        Console.WriteLine("Innlevering registrert.");
+                        break;
+
+                    case "4":
+                        running = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Ugyldig valg.");
+                        break;
+                        }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Feil: {ex.Message}");
+            }
+        }
     }
 }
 
+                
+            
