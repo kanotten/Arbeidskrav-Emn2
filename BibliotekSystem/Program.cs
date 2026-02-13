435f1a01330ddff77ae615e8d501dd66914c17bf
Kenneth Sheikh <kenneth.shekikh@hotmail.com>
2026-02-13 16:01:41 +0100

    added meny clas and first tester

--- BibliotekSystem/Program.cs
+++ BibliotekSystem/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using BibliotekSystem.Models;
+using BibliotekSystem.Services;
+
+class Program
+{
+    static void Main()
+    {
+        Bibliotek bibliotek = new Bibliotek();
+
+        //Test
+        Ansatt ansatt = new Ansatt("Admin", "admin@bibliotek.no");
+        Medlem medlem = new Medlem("Ola Nordmann", "ola@mail.no");
+
+        bibliotek.RegistrerBruker(ansatt);
+        bibliotek.RegistrerBruker(medlem);
+
+        Bok bok1 = new Bok("Haien", 1974, "Peter Benchley", 300);
+        bibliotek.LeggTilMedia(bok1, ansatt);
+    }
+}
+
