e51b79911846960e844fee1331109c62edda5159
Kenneth Sheikh <kenneth.shekikh@hotmail.com>
2026-02-13 12:28:49 +0100

    added forfatter validation and pages integer

--- Models/Bok.cs
+++ Models/Bok.cs
@@ -4,6 +4,19 @@ namespace BibliotekSystem.Models
 {
     public class Bok : Media
     {
+        private string _forfatter;
+        public string _forfatter
+        {
+            get => _forfatter;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Forfatter kan ikke være tom.");
+                _forfatter = value;
+            }
+        }
+
+        public int AntallSider { get; set; }
         
     }
 }
